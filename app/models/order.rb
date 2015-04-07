316fc1c06ae02d36d8a9994df31cc5fcb087cc3e
Will <williamrwilson@gmail.com>
2015-04-07 12:22:05 +0000

    add order total and subtotal

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,12 @@
 class Order < ActiveRecord::Base
   has_many :order_items
+  
+  def total
+    sum = 0
+    self.order_items.each  do |t| 
+      sum += t.subtotal
+    end
+    sum
+  end
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,9 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
   validates :order_id, :product_id, presence: true
+  
+  def subtotal
+    subtotal = self.product.price * self.quantity.to_i
+  end
+  
 end
